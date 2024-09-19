b8a2a1c6a077e89a078dc92ac66721fb3352ed93
tishka322 <tihon1818@yandex.ru>
2024-09-19 18:32:55 +0300

    module6hard

--- module10.1.py
+++ module10.1.py
@@ -6,7 +6,8 @@ from datetime import datetime
 from threading import Thread
 
 # Объявление функции write_words
-def write_words(word_count, file_name):
+def write_words(word_count, file_name): # где word_count - количество записываемых слов,
+    # file_name - название файла, куда будут записываться слова.
     file = open(file_name, 'a', encoding='utf-8')
     for i in range(word_count):
         file.write( f'Какое-то слово №  {i+1}\n')
@@ -14,9 +15,11 @@ def write_words(word_count, file_name):
     file.close()
     print(f'Завершилась запись в файл {file_name}')
 
+# Взятие текущего времени
 time_start = datetime.now()
 
-
+# Запуск функций с аргументами из задачи
+# После создания файла вызовите 4 раза функцию wite_words
 write_words(10, 'example1.txt')
 write_words(30, 'example2.txt')
 write_words(200, 'example3.txt')
@@ -26,12 +29,15 @@ write_words(100, 'example4.txt')
 time_stop = datetime.now()
 time_res = time_stop - time_start
 
+# Вывод разницы начала и конца работы функций
 print(f'Время работы функций {time_res}')
 
+# После вызовов функций создайте 4 потока для вызова этой функции
 
+# Взятие текущего времени
 time2_start = datetime.now()
 
-
+# Создание и запуск потоков с аргументами из задачи
 thr_first = Thread(target=write_words, args= (10, 'example5.txt'))
 thr_second = Thread(target=write_words, args= (30, 'example6.txt'))
 thr_third = Thread(target=write_words, args= (200, 'example7.txt'))
